675b7b038b6908c7be37f402cde2ecef6b59f980
AashvikTyagi <its.aashvik.tyagi@gmail.com>
2024-05-26 12:58:33 +0530

    wrote floodfill

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,18 @@
 #include <string>
 #include <queue>
 
-// direction constants
-#define NORTH 0
+#define NORTH 0 // cardinal constants
 #define SOUTH 1
 #define WEST 2
 #define EAST 3
-
 #define mazeSize 16
-#define forside(sid) for (int sid = 0; sid < mazeSize; sid++) // wonky way
-#define cell_t std::array<int, 2> // cell type, like {row,col}
-std::array rowDelta = {-1,1,0,0}, colDelta = {0,0,-1,1};
-
-// maze[{row,col}] : 0b EWSN EWSN
-std::map<cell_t, uint8_t> maze;
-std::array goal = {8, 7};
+#define forside(sid) for (int sid = 0; sid < mazeSize; sid++) // wonky way to iter over maze
+typedef std::array<int, 2> cell_t;
+cell_t goal = {8, 7};
+std::array rowDelta = {-1,1,0,0}, colDelta = {0,0,-1,1}; // used to get the coords of a neighbor in NSWE
+std::map<cell_t, uint8_t> maze; // holds walls like maze[{row,col}] : 0b EWSN EWSN
+typedef struct {uint8_t dist; uint8_t dir;} cellData_t; // type for a cell's flood data
+std::map<cell_t, cellData_t> flooded;
 
 // direction is 0 1 2 3 [from right caus byte] for NSWE
 void addWall(cell_t cell, uint8_t direction, uint8_t mazeType) {
@@ -48,20 +46,23 @@ void setupMaze() {
   addWall({mazeSize-1,0}, EAST, 0);
 }
 
+// floodfill a maze
 void flood(cell_t target, uint8_t mazeType) {
-  std::map<cell_t, uint8_t> dists;
-  forside(row) forside(col) dists[{row,col}] = 255;
-  dists[target] = 0;
+  forside(row) forside(col) flooded[{row,col}].dist = 255;
+  flooded[target].dist = 0;
   std::priority_queue<cell_t> queue;
   queue.push(target);
   while (queue.size()) {
     cell_t currentCell = queue.top(); queue.pop();
-    for (int direction: {NORTH, SOUTH, WEST, EAST}) {
+    for (uint8_t direction: {NORTH, SOUTH, WEST, EAST}) {
       if (hasExit(currentCell, direction, mazeType)) {
         cell_t nextCell = neighbor(currentCell, direction);
-        uint8_t cost = dists[currentCell] + 1;
-        if (cost<dists[nextCell]) {
-          dists[nextCell] = cost;
+        uint8_t cost = flooded[currentCell].dist;
+        if ((nextCell[0]==7||nextCell[0]==8)&&(nextCell[1]==7||nextCell[1]==8)) cost = 0;
+        else if (direction!=flooded[currentCell].dir) cost += 2;
+        else cost += 1;
+        if (cost<flooded[nextCell].dist) {
+          flooded[nextCell] = {cost, direction};
           queue.push(nextCell);
         }
       }
